bd089174bf7ba09e2d3c7ed64f514decbbee6fdc
luisfga <luisfga@gmail.com>
2020-11-19 08:03:15 -0300

    Correção na atualização do status de mensagens.

--- src/main/java/br/com/luisfga/talkingz/server/database/sql/dao/MessageDAO.java
+++ src/main/java/br/com/luisfga/talkingz/server/database/sql/dao/MessageDAO.java
@@ -106,6 +106,6 @@ public class MessageDAO {
     public void updateMessageStatus(UUID uuid, byte statusMsg){
         Message message = em.find(Message.class, uuid);
         message.msgStatus = statusMsg;
-        em.merge(message);
+        em.persist(message);
     }
 }
